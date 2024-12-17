b571382df9148df52154ca86a988436873969103
andrewPao <andrew_pao_wing_yip@hotmail.com>
2024-12-17 12:15:17 +0800

    initial commit

---
+++ src/main/java/com/game_sale_import/GameSaleImportApplication.java
@@ -0,0 +1,13 @@
+package com.game_sale_import;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameSaleImportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameSaleImportApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/game_sale_import/GameSaleImportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.game_sale_import;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameSaleImportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
